bd37c8900aa56686efd733eb35fc789e9cb71300
Raquel Salva <raquelzssalva@gmail.com>
2017-12-21 15:44:21 -0300

    Script para carga inicial de tabla tabulados

--- install/CambiaEstructura.sql
+++ install/CambiaEstructura.sql
@@ -1,4 +1,4 @@
-set search_path= sigba;
+﻿set search_path= sigba;
 --17/11/09
 alter table valores
     add column g_g_activ text;
@@ -17,3 +17,15 @@ alter table indicadores
 
 --17/11/15
 FALTA AGREGAR LOS GRANT QUE CORRIO EMILIO EN PRODUCCIÓN PARA calcular_totales y diferencia totales (ver dif para_install)
+
+--21/12/2017
+--carga inicial de tabla tabulados
+insert into tabulados(indicador, cortantes)
+  select indicador, cortantes
+  from celdas
+  group by indicador, cortantes
+  order by indicador, cortantes;
+update tabulados t
+  set habilitado=false
+  from (select indicador, nohabilitados, x from indicadores , unnest(nohabilitados)x) as y
+  where t.indicador=y.indicador and t.cortantes=y.x 
