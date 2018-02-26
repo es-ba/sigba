d8b59d640c7b706f7034b2ade04a6591aedc5357
Emilio <emilioplatzer@gmail.com>
2018-02-26 11:47:03 -0300

    Pollyfill de string includes

--- unlogged/menu-2.js
+++ unlogged/menu-2.js
@@ -434,3 +434,18 @@ function getChartBoxes() {
     return document.querySelectorAll('.box-grafico-principal [para-graficador]');
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+    
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
